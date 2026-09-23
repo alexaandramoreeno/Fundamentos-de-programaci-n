a535ccbd89296dabc6cb2539f05c8399cb9fb367
Alexandra Moreno Gonzalez <alexaandramoreeno@gmail.com>
2026-09-23 11:58:11 +0200

    ejercicio5

--- tipos-de-datos-simples/Ejercicio5.py
+++ tipos-de-datos-simples/Ejercicio5.py
@@ -1,5 +1,5 @@
-horas = input("cuantas horas trabajas?")
-coste = input("coste por hora?")
+horas = int(input("cuantas horas trabajas?"))
+coste = float(input("coste por hora?"))
 
 paga = horas *coste
 print (f"Tu paga es: {paga}")
