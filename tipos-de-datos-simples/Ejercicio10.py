77f810291f9bd5fd1a74267669f41386064ad9a9
Alexandra Moreno Gonzalez <alexaandramoreeno@gmail.com>
2026-09-25 12:16:21 +0200

    ejercicio10

---
+++ tipos-de-datos-simples/Ejercicio10.py
@@ -0,0 +1,9 @@
+pesopayaso = 112
+pesomuñeca = 75
+
+cantpayasos = int(input("¿Cuántos payasos?"))
+cantmuñecas = int(input("¿Cuántas muñecas?"))
+
+pesototal = pesopayaso * cantpayasos + pesomuñeca * cantmuñecas
+
+print(f"El peso total enviado es {pesototal} gramos")
