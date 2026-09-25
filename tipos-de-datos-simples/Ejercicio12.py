84203123706137e4ce0325888f236230d3d993aa
Alexandra Moreno Gonzalez <alexaandramoreeno@gmail.com>
2026-09-25 12:36:26 +0200

    ejercicio12

--- tipos-de-datos-simples/Ejercicio12.py
+++ tipos-de-datos-simples/Ejercicio12.py
@@ -4,6 +4,6 @@ panmalo = 3.49 - (3.49 *0.6)
 vendidas = int(input("Barras vendidas que no son del dia: "))
 
 print(f"precio habitual de una barra de pan: {panbueno}")
-print(f"descuento: {panbueno*0.6}")
-print(f"coste final: {panmalo} " )
+print(f"descuento: {round(panbueno*0.6,2)}")
+print(f"coste final: {round(panmalo,2)} " )
 
